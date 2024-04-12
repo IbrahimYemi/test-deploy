185fec920a6fef33b03f4da81431cc58ab5b9d2a
IbrahimYemi <sharafadeen.might@gmail.com>
2024-04-12 07:31:02 +0100

    hero build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/login';
 import LoginRoutes from './routing/LoginRoutes'
@@ -15,31 +12,17 @@ import StartupAnimation from './components/StartupAnimation';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is = {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='min-h-screen bg-white max-w-screen-2xl mx-auto'>
+      <ErrorBoundary>
+        <BrowserRouter>
+          <Navbar />
+          <StartupAnimation />
+        </BrowserRouter>
+        <Footer />
+      </ErrorBoundary>
+    </div >
   )
 }
 
